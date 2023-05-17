0a38202b8fb7989a8acfb9235da22dd252f1dd6e
kevin <kevin,andresfontt@gmail.com>
2023-05-16 21:08:20 -0400

    primer comit app empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'listado de empleados';
+
+  empleados:Empleado[]=[
+
+    new Empleado("Kevin","Fontt","Dios",999999),
+    new Empleado("cum","mckinley","subordinado",999999),
+    new Empleado("scarlett","Fontt","mascota",100),
+    new Empleado("fabian","muñoz","nose",0)
+
+  ]
+
+  agregarEmpleado() {
+    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.empleados.push(miEmpleado)
+  }
+
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
 }

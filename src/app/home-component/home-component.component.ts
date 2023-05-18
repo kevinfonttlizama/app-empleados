8fe6e13e4cbc12c40a7f604b2528f143fbffd4d5
kevin <kevin,andresfontt@gmail.com>
2023-05-18 02:11:14 -0400

    redireccion automatico a home con boton

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Empleado } from '../empleado.model';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { EmpleadosService } from '../empleados.service';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.css']
 })
-export class HomeComponentComponent {
+export class HomeComponentComponent implements OnInit {
+  titulo = 'listado de empleados';
+
+  constructor(private miServicio:ServicioEmpleadosService,private empleadosService:EmpleadosService){
+
+    //this.empleados=this.empleadosService.empleados
+
+  };
+
+
+  ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados
+  }
+
+  empleados:Empleado[]=[];
+
+  agregarEmpleado() {
+    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.miServicio.muestraMensaje("Nombre del empleado:" + miEmpleado.nombre)
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+
+
+  }
+
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
 
 }

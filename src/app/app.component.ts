47aaa6efb2ab4da3b7cf68c6344280892306f19f
kevin <kevin,andresfontt@gmail.com>
2023-05-19 01:36:34 -0400

    añadiendo apy key y authdoDomain del SDK firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import firebase from 'firebase/compat/app'
 
 
 @Component({
@@ -9,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   titulo = 'listado de empleados';
+
+
+
+
+
+
+
+  ngOnInit():void{
+
+    firebase.initializeApp({
+
+      apiKey: "AIzaSyBBUN9SCbsci-vf6OFjD6StfHcUZ2LhplY",
+      authDomain: "mis-clientes-4fd54.firebaseapp.com",
+
+    })
+  }
+
   
   };
 
